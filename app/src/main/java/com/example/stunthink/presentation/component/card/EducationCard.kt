4f8e0ea04d32ff8d185f6840a07e40347c68b15a
muhammad fachri akbar <akbar.fachri@akseleran.com>
2023-06-11 01:13:37 +0700

    Added stunting education mock page

---
+++ app/src/main/java/com/example/stunthink/presentation/component/card/EducationCard.kt
@@ -0,0 +1,68 @@
+package com.example.stunthink.presentation.component.card
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import coil.size.Scale
+import com.example.stunthink.presentation.ui.theme.Typography
+
+@Composable
+fun EducationCard(
+    modifier: Modifier,
+    imageLink: String,
+    title: String,
+    description: String,
+    publishedDate: String,
+    onClick: () -> Unit
+) {
+    OutlinedButton(
+        modifier = modifier,
+        onClick = { onClick() },
+        shape = MaterialTheme.shapes.small,
+        contentPadding = PaddingValues()
+    ) {
+        Column() {
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(imageLink)
+                    .crossfade(true)
+                    .scale(Scale.FILL)
+                    .build(),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+            Column(
+                modifier = Modifier.padding(12.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Text(
+                    text = title,
+                    style = Typography.titleMedium
+                )
+                Text(
+                    text = description,
+                    style = Typography.bodyMedium,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = publishedDate,
+                    style = Typography.labelMedium
+                )
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/stunthink/presentation/screen/main/home/WelcomeScreen.kt
+++ app/src/main/java/com/example/stunthink/presentation/screen/main/home/WelcomeScreen.kt
@@ -1,14 +1,23 @@
 package com.example.stunthink.presentation.screen.main.home
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.stunthink.R
+import com.example.stunthink.presentation.component.card.EducationCard
+import com.example.stunthink.presentation.component.card.MonitoringCard
 import com.example.stunthink.presentation.ui.theme.StunThinkTheme
 import com.example.stunthink.presentation.ui.theme.Typography
 
@@ -17,12 +26,73 @@ fun HomeScreen(
     navController: NavController
 ) {
     StunThinkTheme {
-        Box(modifier = Modifier.fillMaxSize()) {
-            Text(
-                modifier = Modifier.align(Alignment.Center),
-                text = "Beranda",
-                style = Typography.headlineMedium
-            )
+        Box(modifier = Modifier
+            .padding(horizontal = 16.dp)
+            .fillMaxSize()
+        ) {
+            LazyColumn(
+                modifier = Modifier,
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                item {
+                    MonitoringCard(
+                        modifier = Modifier,
+                        title = "Monitoring Anak",
+                        image = R.drawable.mother_baby_illustration,
+                        description = "Belum ada data anak",
+                        buttonText = "Tambah Data Anak"
+                    ) {
+
+                    }
+                }
+                item {
+                    MonitoringCard(
+                        modifier = Modifier,
+                        title = "Monitoring Ibu",
+                        image = R.drawable.mother_illustration,
+                        description = "Belum ada data ibu",
+                        buttonText = "Tambah Data ibu"
+                    ) {
+
+                    }
+                }
+                item {
+                    EducationList()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun EducationList() {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            modifier = Modifier.weight(1f),
+            text = "Edukasi Stunting",
+            style = Typography.headlineSmall
+        )
+        Text(
+            text = "Lihat Semua",
+            style = Typography.bodySmall
+        )
+    }
+    Column(
+        modifier = Modifier.padding(vertical = 16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        (1..2).forEach {
+            EducationCard(
+                modifier = Modifier,
+                imageLink = "https://p2ptm.kemkes.go.id/uploads/VHcrbkVobjRzUDN3UCs4eUJ0dVBndz09/2017/stunting_01.png",
+                title = "Cegah Stunting dengan Perbaikan Pola Makan, Pola Asuh dan Sanitasi",
+                description = "Padahal seperti kita ketahui, genetika merupakan faktor determinan kesehatan yang paling kecil pengaruhnya bila dibandingkan dengan faktor perilaku, lingkungan , dan pelayanan kesehatan. \\\"\\\"Terdapat tiga hal yang harus diperhatikan dalam pencegahan stunting, yaitu perbaikan terhadap pola makan, pola asuh, serta perbaikan sanitasi dan akses air bersih”, tutur Menteri Kesehatan RI, Nila Farid Moeloek, di Jakarta . Diterangkan Menkes Nila Moeloek, kesehatan berada di hilir. Seringkali masalah-masalah non kesehatan menjadi akar dari masalah stunting, baik itu masalah ekonomi, politik, sosial, budaya, kemiskinan, kurangnya pemberdayaan perempuan, serta masalah degradasi lingkungan. Karena itu, ditegaskan oleh Menkes, kesehatan membutuhkan peran semua sektor dan tatanan masyarakat. Pola Makan Masalah stunting dipengaruhi oleh rendahnya akses terhadap makanan dari segi jumlah dan kualitas gizi, serta seringkali tidak beragam. Pola Asuh Bersalin di fasilitas kesehatan, lakukan inisiasi menyusu dini dan berupayalah agar bayi mendapat colostrum air susu ibu Sanitasi dan Akses Air Bersih Rendahnya akses terhadap pelayanan kesehatan, termasuk di dalamnya adalah akses sanitasi dan air bersih, mendekatkan anak pada risiko ancaman penyakit infeksi. \\\"\\\"Pola asuh dan status gizi sangat dipengaruhi oleh pemahaman orang tua maka, dalam mengatur kesehatan dan gizi di keluarganya. Karena itu, edukasi diperlukan agar dapat mengubah perilaku yang bisa mengarahkan pada peningkatan kesehatan gizi atau ibu dan anaknya”, tutupnya.",
+                publishedDate = "7 Juni 2023"
+            ) {
+
+            }
         }
     }
 }
