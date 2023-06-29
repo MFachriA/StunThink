83832091a6aa6a42e7298f88cc6307cff716574c
muhammad fachri akbar <akbar.fachri@akseleran.com>
2023-06-29 22:58:58 +0700

    Split profile screen content

--- app/src/main/java/com/projectAnya/stunthink/presentation/screen/main/profile/ProfileScreen.kt
+++ app/src/main/java/com/projectAnya/stunthink/presentation/screen/main/profile/ProfileScreen.kt
@@ -18,7 +18,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import com.projectAnya.stunthink.R
 import com.projectAnya.stunthink.presentation.navigation.ScreenRoute
 import com.projectAnya.stunthink.presentation.ui.theme.StunThinkTheme
@@ -29,40 +28,48 @@ fun ProfileScreen(
     navController: NavController,
     profileViewModel: ProfileViewModel = hiltViewModel()
 ) {
+    val context = LocalContext.current
+
     StunThinkTheme {
-        val context = LocalContext.current
-        Box(modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-        ) {
-            Text(
-                modifier = Modifier.align(Alignment.Center),
-                text = "Akun",
-                style = Typography.headlineMedium
-            )
-            Button(
-                onClick = {
-                    profileViewModel.deleteUserToken()
-                    navController.navigate(route = ScreenRoute.Welcome.route) {
-                        popUpTo(ScreenRoute.Main.route) { inclusive = true }
-                    }
-                    Toast.makeText(context, R.string.logout_success_message, Toast.LENGTH_LONG).show()
-                },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .align(Alignment.BottomCenter),
-                colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.error)
-            ) {
-                Text(text = stringResource(id = R.string.logout))
+        ProfileScreenContent(
+            logoutCallback = {
+                profileViewModel.deleteUserToken()
+                navController.navigate(route = ScreenRoute.Welcome.route) {
+                    popUpTo(ScreenRoute.Main.route) { inclusive = true }
+                }
+                Toast.makeText(context, R.string.logout_success_message, Toast.LENGTH_LONG).show()
             }
-        }
+        )
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun ProfileScreenPreview() {
-    ProfileScreen(
-        navController = rememberNavController()
-    )
+    ProfileScreenContent(logoutCallback = {})
+}
+
+@Composable
+fun ProfileScreenContent(
+    logoutCallback: () -> Unit
+) {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp)
+    ) {
+        Text(
+            modifier = Modifier.align(Alignment.Center),
+            text = "Akun",
+            style = Typography.headlineMedium
+        )
+        Button(
+            onClick = { logoutCallback() },
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter),
+            colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.error)
+        ) {
+            Text(text = stringResource(id = R.string.logout))
+        }
+    }
 }
